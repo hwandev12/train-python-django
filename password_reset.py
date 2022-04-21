cadb2be1262932cb50d9caeba0253dc4814afd03
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-21 06:14:26 +0500

    password_reset_urls

--- password_reset.py
+++ password_reset.py
@@ -1,4 +1,5 @@
 # This is import from django contribs
+from django.urls import path, include
 from django.contrib.auth.views import(
     LoginView,
     LogoutView,
@@ -7,3 +8,14 @@ from django.contrib.auth.views import(
     PasswordResetConfirmView,
     PasswordResetCompleteView
 )
+# This is how used in the main urls.py file
+urlpatterns = [
+    path('password-reset-view/', PasswordResetView.as_view(),
+         name='password_reset_view'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
+]
