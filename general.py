04cf8b466d4dd8ed2534eefe0cde3efc8051f20f
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-21 06:44:49 +0500

    connect frontend folder to main settings file

--- general.py
+++ general.py
@@ -12,3 +12,22 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
+
+# This is how to connect the outside of the frontend folder
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        # Here we can see
+        'DIRS': [ BASE_DIR / 'frontend' ],
+        # Here we can see
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
