6e97a8dfed3624d1915b56de48204721037a5c74
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-21 06:44:19 +0500

    login, logout urls

--- general.py
+++ general.py
@@ -7,3 +7,8 @@ STATICFILES_DIRS = (
 
 # This is how to connect email send backend here
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# This is how login, logout work with urls here
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/login/'
