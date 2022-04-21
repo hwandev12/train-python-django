c3b36d7f237039b7b223787442ad7bf5a91de432
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-21 06:12:40 +0500

    django password_reset imports

---
+++ password_reset.py
@@ -0,0 +1,9 @@
+# This is import from django contribs
+from django.contrib.auth.views import(
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
