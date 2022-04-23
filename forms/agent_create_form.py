ab8bad03a0b42542c1607d439a8ed290cd516ca9
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-23 06:37:33 +0500

    agent create form

---
+++ forms/agent_create_form.py
@@ -0,0 +1,12 @@
+from django import forms
+from app.model import YourAgent
+
+# It is for create agent in the main agent folder
+class AgentAssignForm(forms.Form):
+    agent = forms.ModelChoiceField(queryset=YourAgent.objects.none())
+
+    def __init__(self, *args, **kwargs):
+        request = kwargs.pop('request')
+        spies = YourAgent.objects.filter(organiser=request.user.userprofile)
+        super(AgentAssignForm, self).__init__(*args, **kwargs)
+        self.fields['agent'].queryset = spies
