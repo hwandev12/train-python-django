58de41afc3374fdd48b2501ba7b23d87449351c5
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-23 06:41:34 +0500

    update

--- forms/registration.py
+++ forms/registration.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm, UsernameField
-from .models import YourUser
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class RegisterForm(UserCreationForm):
     class Meta:
-        model = YourUser
+        model = User
         fields = ("username",)
         field_classes = {'username': UsernameField}
