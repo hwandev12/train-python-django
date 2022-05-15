0de9d3433f8a6ef96f83a12ef88647b9069c5825
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-15 18:48:37 +0500

    first step translation

---
+++ translation/first_try.py
@@ -0,0 +1,9 @@
+# Main settings.py we need to add following codes at first
+
+from django.utils.translation import gettext_lazy as _
+LANGUAGES = [
+    ('uz', _('Uzbek')),
+    ('en', _('English')),
+]
+
+# then we need to add the languages at next
